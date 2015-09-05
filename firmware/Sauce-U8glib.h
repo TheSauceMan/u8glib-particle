8afcfd0783118f86f1e1c36a5bcad0acce69e27e
unknown <sosman@SOSMAN-M3800.am.sony.com>
2015-09-04 17:24:48 -0700

    Couldn't use the name U8glib -- it seems to be taken.
    Changed version number to match the number of the U8glib I forked off of.

---
+++ firmware/Sauce-U8glib.h
@@ -0,0 +1,5 @@
+// This file is here because Particle build wants there to be a .cpp/.h
+// that mach the name of the library.  I did not want to rename the original
+// U8glib files, so I refer to them here.
+
+#include "U8glib.h"
